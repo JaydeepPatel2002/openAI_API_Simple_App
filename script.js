a30842d49b971553e148447eb35a5fb99450d650
Jaydeep k patel <105468488+JaydeepPatel2002@users.noreply.github.com>
2023-05-20 23:22:28 -0600

    open_api

---
+++ script.js
@@ -0,0 +1,66 @@
+document.getElementById('check').addEventListener('click', function() {
+    let inputText = document.getElementById('inputText').value;
+    console.log("==============");
+
+    inputText = inputText + " " + "can you tell me what are the grammar mistakes exists above paragraph or sentence?";
+    console.log(inputText);
+
+    console.log("==============");
+    CallAPI(inputText);
+
+});
+
+document.getElementById('correct').addEventListener('click', function() {
+    let inputText = document.getElementById('inputText').value;
+    console.log("==============");
+
+    inputText = inputText + " " + "can you correct grammar of above paragraph or sentence?";
+    console.log(inputText);
+
+    console.log("==============");
+    CallAPI(inputText);
+
+});
+
+document.getElementById('pro').addEventListener('click', function() {
+    let inputText = document.getElementById('inputText').value;
+    console.log("==============");
+
+    inputText = inputText + " " + "can rewrite above paragraph or sentence more professionally?";
+    console.log(inputText);
+
+    console.log("==============");
+    CallAPI(inputText);
+
+});
+
+
+function CallAPI(input)
+{
+
+
+    fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer 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'
+
+        },
+        body: JSON.stringify({
+            'prompt':input,
+            'max_tokens': 3000,
+            // 'model':'gpt-3.5-turbo',
+            // 'messages':[{"role":"user", "content":"$1"}],
+            'temperature':0.1
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let outputText = data.choices[0].text;
+            document.getElementById('outputText').value = outputText;
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
